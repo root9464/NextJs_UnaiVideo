0ae7b56f89a311751f9831681a52f81855092dcc
io <senpai9464@yandex.ru>
2024-12-19 12:51:22 +0000

    add deploy deps

---
+++ scripts/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DOCKERFILE_PATH="../docker/Dockerfile"
+IMAGE_NAME="unai_video_front"
+CONTAINER_NAME="unai_video_container"
+
+ping() {
+    echo "pong"
+}
+
+rebuild_front() {
+    docker stop $CONTAINER_NAME &&
+    docker rm $CONTAINER_NAME &&
+    docker rmi $IMAGE_NAME &&
+    docker build -f $DOCKERFILE_PATH .. -t $IMAGE_NAME &&
+    docker run --name $CONTAINER_NAME -p 8080:8080 -d $IMAGE_NAME
+}
+
+first_build() {
+    docker build -f $DOCKERFILE_PATH .. -t $IMAGE_NAME &&
+    docker run --name $CONTAINER_NAME -p 8080:8080 -d $IMAGE_NAME
+}
+
+delete_build_bundle(){
+    docker stop $CONTAINER_NAME &&
+    docker rm $CONTAINER_NAME &&
+    docker rmi $IMAGE_NAME
+}
+
+if [ $# -eq 1 ]; then
+    "$1"
+fi
